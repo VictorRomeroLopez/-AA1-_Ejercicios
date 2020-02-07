18e720a889e68850481916d2d4c3c957ea12ceff
VictorRomeroLopez <victor.romero@enti.cat>
2020-02-07 18:32:51 +0100

    Finished Blocking non blocking

--- [AA1]_Ejercicio_1/src/[AA1]_Server.cpp
+++ [AA1]_Ejercicio_1/src/[AA1]_Server.cpp
@@ -1,47 +1,64 @@
 #include <SFML/Network.hpp>
 #include <SFML/System.hpp>
+#include <thread>
+#include <mutex>
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 sf::TcpListener dispatcher;
+std::mutex _mutex;
 
 void print(std::string textToPrint) {
 	std::cout << textToPrint << std::endl;
 }
 
-struct Data {
-public:
-	sf::TcpSocket* socket;
-	std::vector<sf::TcpSocket*> incoming;
-
-	Data(sf::TcpSocket*  _socket, std::vector<sf::TcpSocket*> _incoming) {
-		socket = _socket;
-		incoming = _incoming;
-	}
-	
-	void client() {
-		print("I'm connected " + socket->getRemoteAddress().toString());
-
-		while (true) {
-			sf::Packet packet;
-			socket->receive(packet);
-			std::string packetData;
-			packet >> packetData;
-			print(packetData);
-			std::cout << incoming.size() << std::endl;
-			for (int i = 0; i < incoming.size(); i++) {
-				incoming[i]->send(packet);
-			}
+#pragma region BLOCKING
+bool SendPacketAllClients(sf::Packet packet, std::vector<sf::TcpSocket*>* incoming) {
+	_mutex.lock();
+	sf::Socket::Status status;
+	for (int i = 0; i < incoming->size(); i++) {
+		status = incoming->at(i)->send(packet);
+		if (status != sf::Socket::Done) {
+			incoming->at(i)->disconnect();
+			incoming->erase(incoming->begin() + i);
+			_mutex.unlock();
+			return true;
 		}
-		print("Dissconected");
 	}
-};
+	_mutex.unlock();
+	return false;
+}
 
+void client(sf::TcpSocket* socket, std::vector<sf::TcpSocket*>* incoming) {
+	print("I'm connected " + socket->getRemoteAddress().toString());
+
+	sf::Packet _packet;
+	_packet << "I'm connected " + socket->getRemoteAddress().toString();
+	if (SendPacketAllClients(_packet, incoming)) return;
+
+	while (true) {
+		sf::Packet packet;
+		sf::Socket::Status status = socket->receive(packet);
+		if (status == sf::Socket::Disconnected)
+		{
+			print("Client " + socket->getRemoteAddress().toString() + " disconected!"); 
+			sf::Packet _packet;
+			_packet << "Client " + socket->getRemoteAddress().toString() + " disconected!";
+			if(SendPacketAllClients(_packet, incoming)) return;
+		}
+		if (SendPacketAllClients(packet, incoming)) return;
+	}
+	print("Dissconected");
+}
+
+#pragma endregion
 
 int main()
 {
+	using namespace std::chrono_literals;
 	std::string serverMethod;
-	std::vector<sf::TcpSocket*> incoming;
+	std::vector<sf::TcpSocket*> *incoming = new std::vector<sf::TcpSocket*>();
 
 	do {
 		print("Selecciona un metode:");
@@ -64,30 +81,54 @@ int main()
 
 	print("Succesfully linked to port " + std::to_string(port));
 
+	sf::TcpSocket* socket;
+	sf::Packet packet;
 	while (true) {
 
-		sf::TcpSocket* socket = new sf::TcpSocket();
-		if (dispatcher.accept(*socket) != sf::Socket::Done) {
-			print("Error al aceptar la conexion");
+		socket = new sf::TcpSocket();
+		status = dispatcher.accept(*socket);
+
+		if (status == sf::Socket::NotReady) {
 			socket = NULL;
 			delete socket;
 		}
-		else 
+		else if( status == sf::Socket::Done)
 		{
-			incoming.push_back(socket);
+			_mutex.lock();
+			incoming->push_back(socket);
+			_mutex.unlock();
+
 			switch (std::stoi(serverMethod)) {
-			case 1: 
-			{
-				Data* data = new Data(socket, incoming);
-				sf::Thread thread( &Data::client, data);
-				thread.launch();
-				break;
-			}
-			case 2:
-				break;
-			case 3:
-				break;
+				case 1: 
+				{
+					std::thread threadstd(&client, socket, incoming);
+					threadstd.detach();
+					socket = NULL;
+					break;
+				}
+				case 3:
+					break;
 			}
 		}
+
+		if (std::stoi(serverMethod) == 2 && incoming->size() > 0) {
+			for (int i = 0; i < incoming->size(); i++) {
+				incoming->at(i)->setBlocking(false);
+				status = incoming->at(i)->receive(packet);
+				if (status == sf::Socket::Disconnected)
+				{
+					print("Client " + incoming->at(i)->getRemoteAddress().toString() + " disconected!");
+					sf::Packet _packet;
+					_packet << "Client " + incoming->at(i)->getRemoteAddress().toString() + " disconected!";
+					SendPacketAllClients(_packet, incoming);
+				}
+				else if (status == sf::Socket::Done) {
+					SendPacketAllClients(packet, incoming);
+				}
+			}
+			std::this_thread::sleep_for(1s);
+		}
 	}
+
+	return 0;
 }
